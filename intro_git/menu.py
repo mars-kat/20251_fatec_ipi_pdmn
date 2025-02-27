0e1063c8ca76b7ddc2ccc0d4a6077b40f440b04b
Katia Martins <katiamartins13@yahoo.com.br>
2025-02-27 19:54:38 -0300

    feat: implementa opcao divisao

--- intro_git/menu.py
+++ intro_git/menu.py
@@ -6,6 +6,7 @@ def menu(a, b):
         print("[1] Somar")
         print("[2] Subtrair")
         print("[3] Multiplicar")
+        print("[4] Dividir")
         escolha = input("Digite o número da opção desejada: ")
         
         if escolha == "1":
@@ -14,3 +15,5 @@ def menu(a, b):
             print(f'{a} - {b} = {calculadora.subtrair(a, b)}')
         elif escolha == "3":
             print(f'{a} * {b} = {calculadora.multiplicar(a, b)}')
+        elif escolha == "4":
+            print(f'{a} / {b} = {calculadora.dividir(a, b)}')
