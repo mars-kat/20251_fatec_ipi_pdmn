21a71a9ffa80a7de5f956fdc403f3bd4aa990adb
Katia Martins <katiamartins13@yahoo.com.br>
2025-02-27 19:48:34 -0300

    feat: implementa opcao subtracao

--- intro_git/menu.py
+++ intro_git/menu.py
@@ -4,7 +4,10 @@ def menu(a, b):
     while True:
         print("Menu Calculadora:")
         print("[1] Somar")
+        print("[2] Subtrair")
         escolha = input("Digite o número da opção desejada: ")
         
-        if escolha == '1':
+        if escolha == "1":
             print(f'{a} + {b} = {calculadora.somar(a, b)}')
+        elif escolha == "2":
+            print(f'{a} - {b} = {calculadora.subtrair(a, b)}')
